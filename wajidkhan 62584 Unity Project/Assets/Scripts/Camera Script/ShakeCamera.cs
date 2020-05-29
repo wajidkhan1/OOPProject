b243061269c3676960365734a0fb9588b7dfd337
wajid khan <64194854+wajidkhan1@users.noreply.github.com>
2020-05-29 17:32:16 +0500

    OOP Project
    OOP Project

---
+++ wajidkhan 62584 Unity Project/Assets/Scripts/Camera Script/ShakeCamera.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour
+{
+
+    public float power = 0.2f;
+    public float duration = 0.2f;
+    public float slowDownAmount = 1f;
+    private bool should_Shake;
+    private float initialDuration;
+
+    private Vector3 startPosition;
+
+    void start()
+    {
+        startPosition = transform.localPosition;
+        initialDuration = duration;
+    }
+
+    
+    // Update is called once per frame
+    void Update(){
+        Shake();
+    }
+    void Shake()
+    {
+        if (should_Shake)
+        {
+            if (duration > 0f)
+            {
+                transform.localPosition = startPosition + Random.insideUnitSphere * power;
+                duration -= Time.deltaTime * slowDownAmount;
+
+            }
+            else
+            {
+                should_Shake = false;
+                duration = initialDuration;
+                transform.localPosition = startPosition;
+            }
+        }
+    }
+             
+         
+         
+              public bool  ShouldShake {
+                  get{
+                      return should_Shake;
+                  }
+                  set{
+                      should_Shake=value;
+                  }
+              
+    
+           
+    }
+}
---
+++ wajidkhan 62584 Unity Project/Assets/Scripts/Player Script/PlayerAttack.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ComboState
+{
+    NONE,
+    PUNCH_1,
+    PUNCH_2,
+    PUNCH_3,
+    KICK_1,
+    KICK_2
+}
+
+    
+public class PlayerAttack : MonoBehaviour
+{
+    private CharacterAnimation player_Anim;
+
+    private bool ActivateTimerToReset;
+
+    private float default_Combo_Timer = 0.4f;
+    private float current_Combo_Timer;
+
+    private ComboState current_Combo_State;
+
+    void Awake()
+    {
+        player_Anim = GetComponentInChildren<CharacterAnimation>();
+    }
+
+
+    void Start()
+    {
+        current_Combo_Timer = default_Combo_Timer;
+        current_Combo_State = ComboState.NONE;
+    }
+
+    //update is called once per frame
+    void Update()
+    {
+        ComboAttacks();
+        ResetComboState();
+    }
+
+    void ComboAttacks()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (current_Combo_State == ComboState.PUNCH_3 ||
+                current_Combo_State == ComboState.KICK_1 ||
+                current_Combo_State == ComboState.KICK_2)
+                return;
+
+            current_Combo_State++;
+            ActivateTimerToReset = true;
+            current_Combo_Timer = default_Combo_Timer;
+
+            if (current_Combo_State == ComboState.PUNCH_1)
+            {
+                player_Anim.Punch_1();
+            }
+
+            if (current_Combo_State == ComboState.PUNCH_2)
+            {
+                player_Anim.Punch_2();
+            }
+
+            if (current_Combo_State == ComboState.PUNCH_3)
+            {
+                player_Anim.Punch_3();
+            }
+
+        }  // if punch
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+
+
+            // if the current combo is punch 3 or kick2
+            //return bcz we have no combos to perfom
+             if (current_Combo_State == ComboState.KICK_2 ||
+                 current_Combo_State == ComboState.PUNCH_3)
+                      return;
+            // if the current combo state is NONE, OR PUNCH1 OR PUNCH2
+            //THEN WE CAN SET CURRENT COMBO STATE TO KICK1 TO CHAIN THE COMBO
+            if (current_Combo_State == ComboState.NONE ||
+                current_Combo_State == ComboState.PUNCH_1 ||
+                current_Combo_State == ComboState.PUNCH_2) {
+                    current_Combo_State = ComboState.KICK_1;
+                } else if(current_Combo_State == ComboState.KICK_1) {
+                    //MOVE TO KICK2
+                    current_Combo_State++;
+                }
+
+            ActivateTimerToReset = true;
+            current_Combo_Timer = default_Combo_Timer;
+
+            if (current_Combo_State == ComboState.KICK_1)
+            {
+                player_Anim.Kick_1();
+            }
+
+            if (current_Combo_State == ComboState.KICK_2)
+            {
+                player_Anim.Kick_2();
+            }
+
+
+        } //if kick
+
+    }
+
+
+        //combo attacks
+    void ResetComboState()
+    {
+        if (ActivateTimerToReset)
+        {
+            current_Combo_Timer -= Time.deltaTime;
+            if (current_Combo_Timer <= 0f)
+            {
+                current_Combo_State = ComboState.NONE;
+
+                ActivateTimerToReset = false;
+                current_Combo_Timer = default_Combo_Timer;
+
+    }
+
+
+
+}}}
